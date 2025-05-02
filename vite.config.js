f07595904b9209f7c6974541b917ec8f52af1ab4
Avika2211 <avikaintern@gmail.com>
2025-05-02 20:19:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/threejs-portfolio/', // use your repo name
+  plugins: [react()]
 })
